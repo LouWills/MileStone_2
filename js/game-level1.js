9c56522a4ca7e29462582e87ee68c3d5430365a0
LouWills <52356181+LouWills@users.noreply.github.com>
2019-09-10 13:59:09 +0200

    Trying to get modal to work

--- js/game-level1.js
+++ js/game-level1.js
@@ -1,6 +1,7 @@
 //Memory Game based of off https://github.com/taniarascia/memory
 
 // playing card data
+// Loading playing card data into an array
 const cardsArray = [{
     name: 'joker',
     img: 'png/black_joker.png',
@@ -28,41 +29,55 @@ const cardsArray = [{
 
 ]
 
+//concatenating card array to create pairs
+//Randomly sorting the array using math.random so cards dont appear in the same location each round you play
 const gameGrid = cardsArray
 .concat(cardsArray)
 .sort(() => 0.5 - Math.random());
 
+//declaring variables
 let firstGuess = '';
 let secondGuess = '';
 let count = 0;
 let previousTarget = null;
 let delay = 1200;
+
+//Turn counter variable doesn't work...yet!!
 let turnCounter = 0;
 
+//creating a html section in the div with id 'game' to be used for the game grid.
+//setting the section the class 'lvl1 grid'
 const game = document.getElementById('game');
 const grid = document.createElement('section');
 grid.setAttribute('class', 'lvl1-grid');
 game.appendChild(grid);
 
+//
 gameGrid.forEach(item => {
 const { name, img } = item;
 
+//creating the cards for the game. a div with the class of 'card' 
 const card = document.createElement('div');
 card.classList.add('card');
 card.dataset.name = name;
 
+//the front of the card (the back of the playing card) 
 const front = document.createElement('div');
 front.classList.add('front');
 
+//using template literal to make the back of the game card an image in the array
 const back = document.createElement('div');
 back.classList.add('back');
 back.style.backgroundImage = `url(${img})`;
 
+//appending to the grid a card
+//appending to each card a front element and a back element
 grid.appendChild(card);
 card.appendChild(front);
 card.appendChild(back);
 });
 
+//
 const match = () => {
 const selected = document.querySelectorAll('.selected');
 selected.forEach(card => {
@@ -70,6 +85,7 @@ card.classList.add('match');
 });
 };
 
+//resetting the variables if the two selected cards are not a match
 const resetGuesses = () => {
 firstGuess = '';
 secondGuess = '';
@@ -118,3 +134,12 @@ previousTarget = clicked;
 }
 
 });
+
+//Trying to check if all the child element of the game grid have the class of match
+//(Check if all game cards have been matched)
+//Then show a victory bootstrap modal
+let gameParent = document.querySelector(".lvl1-grid");
+
+if (gameParent.children.length == gameParent.querySelectorAll(".match").length) {
+  $('#exampleModal').show(fast);
+}
